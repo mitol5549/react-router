e7dc0a7c6031db5d08eb936abb8937ef40e9359c
Andrey Osipov <mitol5549@gmail.com>
2023-07-18 16:39:59 +0200

    defining Routers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/products',
+    element: <ProductsPage />,
+  },
+]);
+
 const App = () => {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
